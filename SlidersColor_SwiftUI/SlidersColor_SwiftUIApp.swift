e352be69b401bee8df903c73f494be702306a717
AlexeyB163 <sourceaisa@mail.ru>
2022-04-03 10:57:38 +0400

    Initial Commit

---
+++ SlidersColor_SwiftUI/SlidersColor_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidersColor_SwiftUIApp.swift
+//  SlidersColor_SwiftUI
+//
+//  Created by User on 03.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SlidersColor_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
